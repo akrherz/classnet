6d2cf65624741192fe33d2600b8a71426c2398fc
akrherz <akrherz@iastate.edu>
2026-01-12 22:40:55 -0600

    🎨 Add some more debugging

--- cgi-bin/lib/ERROR.pm
+++ cgi-bin/lib/ERROR.pm
@@ -326,29 +326,31 @@ Just dies
 
 sub system_error {
     my ($file_name,$sub_name,$op_name,@other) = @_;
+    my $sys_err = $! ? " (system error: $!)" : "";
     print_error_header(${$CNERROR});
     print "$err_msg{$CNERROR}\n";
+    print "<BR><b>System error:</b> $!<BR>" if $!;
     print <<"FOOTER";
     <CENTER>$GLOBALS::HR</CENTER>
     </BODY>
     </HTML>
 
-FOOTER
+  FOOTER
 
     $logname = "$GLOBALS::SERVER_LOG_DIR/classnet.log";
     open(ERR_LOG, ">>$logname");
     print ERR_LOG "----------\n";
     #$tm = &ctime::ctime(time);
     print ERR_LOG "$tm";
-    print ERR_LOG "${file_name}::$sub_name($op_name)\n";
+    print ERR_LOG "${file_name}::$sub_name($op_name)$sys_err\n";
     print ERR_LOG "Other: @other\n";
     $i = 0;
     while (($pack,$file,$line,$subname) = caller($i++)) {
-        print ERR_LOG "$pack,$file,$line,$subname\n";
+      print ERR_LOG "$pack,$file,$line,$subname\n";
     }
     close(ERR_LOG);
     CN_UTILS::mail($GLOBALS::SYSTEM_EMAIL,'ClassNet System Error',
-    "--------\n${file_name}::$sub_name($op_name)\n@other\n");
+    "--------\n${file_name}::$sub_name($op_name)$sys_err\n@other\n");
     die;
 }
 
